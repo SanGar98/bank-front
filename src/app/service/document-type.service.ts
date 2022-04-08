e9085dab81d8485fecaadecbbf753f463cd53340
SanGar98 <sgm2798@gmail.com>
2022-04-07 19:12:35 -0500

    Carga inicial de la primera version

---
+++ src/app/service/document-type.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { DocumentType } from '../domain/document-type';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DocumentTypeService {
+
+  url:string=`${environment.apiUrl}/api/v1/document-types`
+
+  constructor(public httpClient:HttpClient) { }
+
+  findAll():Observable<DocumentType[]>{
+    return this.httpClient.get<DocumentType[]>(this.url);
+  }
+
+  findById(id:number):Observable<DocumentType>{
+    return this.httpClient.get<DocumentType>(`${this.url}/${id}`);
+  }
+}
